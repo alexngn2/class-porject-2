f6e701fa017d28162295056147d1b8871ef7bd3f
sk <sk2060sk2060@gmail.com>
2020-03-26 21:22:56 -0600

    Worked on html routes & file structuring

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
-const WebSocket = require("ws");
-const socket = new WebSocket('wss://ws.finnhub.io?token=');
+// const WebSocket = require("ws");
+// const socket = new WebSocket('wss://ws.finnhub.io?token=');
 
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+// var db = require("./models");
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +12,10 @@ app.use(express.static("public"));
 
 //add passport here if decided
 
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 // require("./routes/api-routes.js")(app);
 
-db.sequelize.sync().then(function () {
+// db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log(
             "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
@@ -23,22 +23,22 @@ db.sequelize.sync().then(function () {
             PORT
         );
     });
-});
+// });
 
 // Connection opened -> Subscribe
-socket.addEventListener('open', function (event) {
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
-});
+// socket.addEventListener('open', function (event) {
+//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
+// });
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
+// // Listen for messages
+// socket.addEventListener('message', function (event) {
+//     console.log('Message from server ', event.data);
+// });
 
-// Unsubscribe
- var unsubscribe = function(symbol) {
-    socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
-}
+// // Unsubscribe
+//  var unsubscribe = function(symbol) {
+//     socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+// }
 
