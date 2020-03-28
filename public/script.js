1e266b9b492531901e7e3d9a25519ed2945698ff
Ginseng <esullivann@hotmail.com>
2020-03-28 12:42:28 -0600

    updating front-end stock api calls

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 var APIKey = "bpulodvrh5rdgi0uf2ug";
 var queryURL = "https://finnhub.io/api/v1" + "" + "&token=" + APIKey;
-var stockSearch = "/stock/option-chain?symbol="; //
 var searchSymbol; //for search bar, when user imputs stock symbol
 var stockSymbolSearch = "/stock/symbol?exchange=US"; //so user can search for symbol with stock name
 var stockTrends = "/stock/recommendation?symbol=" + stockSymbol; //stock recommendations to buy/sell, etc.
@@ -8,6 +7,7 @@ var stockSymbol;
 
 //search for symbol
 function getSymbol(){
+  var stockSearch = document.getElementById("searchBar").value;
     console.log("running");
     $.ajax({
       type: "GET",
@@ -21,6 +21,8 @@ function getSymbol(){
       contentType: 'application/json',
       success: function(data) {
           console.log(data);
+      var stockName = data[0,1,2].displaySymbol;
+      console.log(stockName);
       
       },  
       fail: function(error) {
@@ -28,4 +30,29 @@ function getSymbol(){
       } 
     });
     };
+
+    function getRecommendations(){
+        console.log("rec");
+        $.ajax({
+          type: "GET",
+          data: {
+              apikey:"bpulodvrh5rdgi0uf2ug",
+              format:"json",
+          },
+        
+          url: "https://finnhub.io/api/v1/" + stockTrends + "&token=" + APIKey;
+          dataType: "json",
+          contentType: 'application/json',
+          success: function(data) {
+              console.log(data);
+          //var stockName = data.message.body.displaySymbol[0,1,2];
+          //console.log(stockName);
+          
+          },  
+          fail: function(error) {
+            console.log(error);
+          } 
+        });
+      };
+
     getSymbol();
